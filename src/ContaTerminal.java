d78cf48f54c1b09a83a261a2feac72a9f6080ad2
Matheus Galimberti <matheus.padilhagalimberti@gmail.com>
2026-01-15 19:44:28 -0300

    Recebe dados e Imprime no Terminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.print("Por favor digite o numeoro da agência: ");
+        numero = sc.nextInt();
+        System.out.print("digite o nome da agencia :");
+        agencia = sc.next();
+        System.out.print("digite seu nome por favor: ");
+        nomeCliente = sc.next();
+        System.out.print("digite seu saldo: ");
+        saldo = sc.nextDouble();
+
+        System.out.printf("Ola %s, obrigado por criar uma conta no nosso banco, sua agência é %s, conta %d," +
+                " e seu saldo é %.2f já esta diponivel para saque", nomeCliente, agencia, numero,saldo);
+
+        sc.close();
+    }
+
+
+}
